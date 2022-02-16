fdfd2e0fac6a80baebb2f5ba1432c810cb43cbf0
Kampaku <78536944+Kirinochka@users.noreply.github.com>
2022-02-16 14:22:20 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import GameSavingLoader from './js/GameSavingLoader';
 import './css/style.css';
 
 // TODO: write your code in app.js
+GameSavingLoader.load()
+  .then((saving) => console.log(saving));
--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,11 +1,13 @@
 import json from './parser';
 import read from './reader';
+import GameSaving from './GameSaving';
 
 class GameSavingLoader {
   static load() {
     const data = read(); // возвращается Promise!
     const value = data.then((res) => json(res)); // возвращается Promise!
-    return value;
+    return new Promise((resolve) => resolve(new GameSaving(value)))
+      .then((res) => res.save);
   }
 }
 
